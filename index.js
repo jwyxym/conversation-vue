0bb8e2bd48ae31b3794b576b6d721e32a32b099c
jwyxym <jwyxym@126.com>
2025-09-28 17:24:04 +0800

    fix

---
+++ index.js
@@ -0,0 +1,5 @@
+import ConversationBlock from './src/components/conversationBlock.vue';
+import ConversationWrapper from './src/components/conversationWrapper.vue';
+import MsgPop from './src/components/msgItem.vue';
+
+export { ConversationBlock, ConversationWrapper, MsgPop };
